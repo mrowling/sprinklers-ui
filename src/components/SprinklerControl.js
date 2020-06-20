4808320e6e022ff0a3685d0a5a12aeeb281f690e
mrowlingfox <mitchell.rowling@foxsports.com.au>
2020-06-21 02:50:52 +1000

    WIP Sprinkler Display/Control

--- src/components/SprinklerControl.js
+++ src/components/SprinklerControl.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Box from "@material-ui/core/Box";
 import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
 import CardHeader from "@material-ui/core/CardHeader";
 import InvertColorsIcon from "@material-ui/icons/InvertColors";
 
@@ -9,7 +9,7 @@ import SprinkerButton from "./SprinklerButton";
 import { useAuth0 } from "../react-auth0-spa";
 import { getApi, putApi } from "../utils/callApi";
 
-function SprinklerDisplay(props) {
+function SprinklerControl(props) {
   const { fullName, shortName } = props;
   const [running, setRunning] = useState(false);
   const { getIdTokenClaims } = useAuth0();
@@ -24,13 +24,12 @@ function SprinklerDisplay(props) {
     <Box width={1 / 4}>
       <Card>
         <CardHeader title={fullName} />
-        <CardContent>
-          {running && <InvertColorsIcon color="primary" />}
-          {!running && <InvertColorsIcon color="secondary" />}
-        </CardContent>
+        <CardActions>
+          <SprinkerButton callApi={triggerSprinkler} />
+        </CardActions>
       </Card>
     </Box>
   );
 }
 
-export default SprinklerDisplay;
+export default SprinklerControl;
