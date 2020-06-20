6d6a516e846f4b2a5bf6e7f268112dc23956f5fa
mrowlingfox <mitchell.rowling@foxsports.com.au>
2020-06-21 02:50:07 +1000

    Use large button again

--- src/components/SprinklerButton.js
+++ src/components/SprinklerButton.js
@@ -4,7 +4,12 @@ import { Button, Grid } from "@material-ui/core";
 function SprinklerButton(props) {
   const { callApi } = props;
   return (
-    <Button onClick={() => callApi()} color="primary" fullWidth={true}>
+    <Button
+      onClick={() => callApi()}
+      size="large"
+      color="primary"
+      fullWidth={true}
+    >
       Trigger
     </Button>
   );
