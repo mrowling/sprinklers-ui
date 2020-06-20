fcfa66e50450636fb010d7002d6b3861f9d481d8
mrowlingfox <mitchell.rowling@foxsports.com.au>
2020-06-21 02:49:13 +1000

    Grid adjustments

--- src/components/SprinklerPage.js
+++ src/components/SprinklerPage.js
@@ -2,30 +2,30 @@ import React from "react";
 import { Grid } from "@material-ui/core";
 
 import SprinkerDisplay from "./SprinklerDisplay";
+import SprinkerControl from "./SprinklerControl";
 
 const SprinklerPage = () => {
   return (
     <>
       <Grid container spacing={3}>
-        <Grid container item justify="space-evenly">
+        <Grid container item justify="space-evenly" alignItems="center">
           <SprinkerDisplay fullName="NW" shortName="nw" />
           <SprinkerDisplay fullName="NE" shortName="ne" />
-          <SprinkerDisplay fullName="N" shortName="northGroup" />
+          <SprinkerControl fullName="N" shortName="nw" />
         </Grid>
-        <Grid container item justify="space-evenly">
+        <Grid container item justify="space-evenly" alignItems="center">
           <SprinkerDisplay fullName="CW" shortName="cw" />
           <SprinkerDisplay fullName="CE" shortName="ce" />
-          <SprinkerDisplay fullName="C" shortName="centreGroup" />
+          <SprinkerControl fullName="C" shortName="centreGroup" />
         </Grid>
-        <Grid container item justify="space-evenly">
+        <Grid container item justify="space-evenly" alignItems="center">
           <SprinkerDisplay fullName="SW" shortName="sw" />
           <SprinkerDisplay fullName="SE" shortName="se" />
-          <SprinkerDisplay fullName="S" shortName="southGroup" />
+          <SprinkerControl fullName="S" shortName="southGroup" />
         </Grid>
-        <Grid container item justify="space-evenly">
-          <SprinkerDisplay fullName="E" shortName="eastGroup" />
-          <SprinkerDisplay fullName="W" shortName="westGroup" />
-          <SprinkerDisplay fullName="All" shortName="allGroup" />
+        <Grid container item justify="space-evenly" alignItems="center">
+          <SprinkerControl fullName="E" shortName="eastGroup" />
+          <SprinkerControl fullName="W" shortName="westGroup" />
         </Grid>
       </Grid>
     </>
