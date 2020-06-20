9382ad9b6d039b61c4a372d85f3446fe741ed6e4
mrowlingfox <mitchell.rowling@foxsports.com.au>
2020-06-21 09:26:53 +1000

    Move Pump State higher

--- src/components/PumpDisplay.js
+++ src/components/PumpDisplay.js
@@ -7,28 +7,8 @@ import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Countdown from "react-countdown";
 
-import { useAuth0 } from "../react-auth0-spa";
-import { getApi } from "../utils/pumpApi";
-
 function PumpDisplay(props) {
-  const { name } = props;
-  const [running, setRunning] = useState(false);
-  const [estimatedEndTime, setEstimatedEndTime] = useState();
-  const { getIdTokenClaims } = useAuth0();
-
-  const getState = async () => {
-    const responseData = await getApi(getIdTokenClaims);
-    const { running, estimated_end_time } = responseData;
-    setRunning(running);
-    setEstimatedEndTime(estimated_end_time);
-  };
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      getState();
-    }, 5000);
-    return () => clearInterval(interval);
-  }, []);
+  const { name, estimatedEndTime, running } = props;
   const countdownRender = ({ minutes, seconds, completed }) =>
     completed ? null : `${minutes}:${seconds}`;
 
--- src/components/SprinklerPage.js
+++ src/components/SprinklerPage.js
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Grid } from "@material-ui/core";
 
-import SprinkerDisplay from "./PumpDisplay";
+import PumpDisplay from "./PumpDisplay";
 import SprinkerControl from "./SprinklerControl";
 
+import { useAuth0 } from "../react-auth0-spa";
+import { getApi } from "../utils/pumpApi";
+
 const SprinklerPage = () => {
+  const [running, setRunning] = useState(false);
+  const [estimatedEndTime, setEstimatedEndTime] = useState();
+  const { getIdTokenClaims } = useAuth0();
+
+  const getPumpData = async () => {
+    const responseData = await getApi(getIdTokenClaims);
+    const { running, estimated_end_time } = responseData;
+    setRunning(running);
+    setEstimatedEndTime(estimated_end_time);
+  };
+
+  useEffect(() => {
+    getPumpData();
+    const interval = setInterval(() => {
+      getPumpData();
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       <Grid container spacing={3}>
         <Grid container item justify="space-evenly" alignItems="center">
-          <SprinkerDisplay name="Pump" />
+          <PumpDisplay
+            name="Pump"
+            estimatedEndTime={estimatedEndTime}
+            running={running}
+          />
         </Grid>
         <Grid container item justify="space-evenly" alignItems="center">
           <SprinkerControl fullName="North" shortName="north" />
