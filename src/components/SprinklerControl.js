ca32d13f16e284f644c4301675c77e8131ac5425
mrowlingfox <mitchell.rowling@foxsports.com.au>
2020-06-21 06:22:00 +1000

    Basic Alerts

--- src/components/SprinklerControl.js
+++ src/components/SprinklerControl.js
@@ -3,6 +3,8 @@ import Box from "@material-ui/core/Box";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardHeader from "@material-ui/core/CardHeader";
+import { Alert } from "@material-ui/lab";
+import Snackbar from "@material-ui/core/Snackbar";
 
 import SprinkerButton from "./SprinklerButton";
 import { useAuth0 } from "../react-auth0-spa";
@@ -10,24 +12,45 @@ import { putApi } from "../utils/sprinklerApi";
 
 function SprinklerControl(props) {
   const { fullName, shortName } = props;
-  const [running, setRunning] = useState(false);
   const { getIdTokenClaims } = useAuth0();
+  const [snackBarMessage, setSnackBarMessage] = useState(false);
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setSnackBarMessage(false);
+  };
 
   const triggerSprinkler = async () => {
+    handleClose();
     const responseData = await putApi(getIdTokenClaims, `/${shortName}`);
-    const { running } = responseData;
-    setRunning(running);
+    const { running, success, message } = responseData;
+    if (!success) {
+      setSnackBarMessage(message || "An Error Occured");
+    }
   };
 
   return (
-    <Box width={1 / 4}>
-      <Card>
-        <CardHeader title={fullName} />
-        <CardActions>
-          <SprinkerButton callApi={triggerSprinkler} />
-        </CardActions>
-      </Card>
-    </Box>
+    <React.Fragment>
+      <Box width={1 / 4}>
+        <Card>
+          <CardHeader title={fullName} />
+          <CardActions>
+            <SprinkerButton callApi={triggerSprinkler} />
+          </CardActions>
+        </Card>
+      </Box>
+      <Snackbar
+        open={snackBarMessage}
+        autoHideDuration={6000}
+        onClose={handleClose}
+      >
+        <Alert onClose={handleClose} severity="error">
+          {snackBarMessage}
+        </Alert>
+      </Snackbar>
+    </React.Fragment>
   );
 }
 
