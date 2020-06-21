53fe87b0dd47e405a6afbb4ab609b0009401e9e4
mrowlingfox <mitchell.rowling@foxsports.com.au>
2020-06-21 16:17:45 +1000

    Updates

---
+++ precache-manifest.e90a9816fbb3cca8cc7c8fcc58939516.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7851db7e6805fc815fb68b6df70a4e3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0b8d1409a6ed2ae3591",
+    "url": "/static/css/main.c445461d.chunk.css"
+  },
+  {
+    "revision": "a75e735dcea8d462ef0e",
+    "url": "/static/js/2.b29d29cd.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.b29d29cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0b8d1409a6ed2ae3591",
+    "url": "/static/js/main.053ba2de.chunk.js"
+  },
+  {
+    "revision": "64a3cb0f3b6ac86e4af0",
+    "url": "/static/js/runtime-main.23ac1ed0.js"
+  }
+]);
